64ed4a36b987cc2e91958263bc369038041a6efe
17thaugust <78376364+17thaugust@users.noreply.github.com>
2021-03-19 23:17:13 +0100

    Created forecast code for next hours

--- src/app.js
+++ src/app.js
@@ -1,5 +1,13 @@
 function formatDate(timestamp) {
     let date = new Date(timestamp);
+   
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let day = days[date.getDay()];
+    return `${day} ${formatHours(timestamp)}`;
+}
+
+function formatHours(timestamp) {
+    let date = new Date(timestamp);
     let hours = date.getHours();
     if (hours < 10) {hours = `0${hours}`;
 }
@@ -7,9 +15,7 @@ function formatDate(timestamp) {
     if (minutes < 10) {minutes = `0${minutes}`;
 }
 
-    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    let day = days[date.getDay()];
-    return `${day} ${hours}:${minutes}`;
+    return `${hours}:${minutes}`;
 }
 
 function displayTemperature(response) {
@@ -34,10 +40,36 @@ function displayTemperature(response) {
 
 }
 
+function displayForecast(response) {
+    let forecastElement = document.querySelector("#forecast");
+    forecastElement.innerHTML = null;
+    let forecast = null;
+
+for (let index = 0; index < 6; index++) {
+ forecast = response.data.list[index];
+ forecastElement.innerHTML += `
+      <div class="col-2">
+    <h3>${formatHours(forecast.dt * 1000)}
+    </h3>
+    <img src="http://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png" alt="" />
+    <div class="weather-forecast-temperature">
+      <strong>${Math.round(forecast.main.temp_max)}°</strong> ${Math.round(forecast.main.temp_min)}°
+    </div>
+  </div>
+    `;  
+    
+}
+
+}
+
 function search(city) {
   let apiKey = "09a78485014abc9ba4bba90aeb20d6e1";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
+
+apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(displayForecast);
+
 }
 
 function handleSubmit(event) {
